36761e5e0205c7e5e9804bbc47f7889c85c57a47
HowardMak <howie1113@gmail.com>
2024-10-09 22:09:31 +0930

    - Modify the animation file

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -3,7 +3,8 @@ import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
 
 const spinAnimation = keyframes`
     0% {transform: rotate(0deg)}
-    100% {transform: rotate(0deg)}
+    50% {transform: rotate(180deg)}
+    100% {transform: rotate(360deg)}
 `;
 
 export const SpinningHourglass = styled(HourglassBottomIcon)(({ theme }) => ({
