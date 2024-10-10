103911f7c368d491c809b16d49b586fbb8fca239
HowardMak <howie1113@gmail.com>
2024-10-10 12:01:15 +0930

    - Updated src/modules/community/SelectTags.tsx

--- src/modules/community/SelectTags.tsx
+++ src/modules/community/SelectTags.tsx
@@ -1,13 +1,14 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { useQuery } from "@apollo/client";
 import { GET_TAGS } from "@/graphql/qa";
 import { TransitionProps } from "@mui/material/transitions";
-import { Avatar, Box, Button, Checkbox, Dialog, DialogContent, DialogTitle, List, ListItem, Typography } from "@mui/material";
+import { Avatar, Box, Button, Checkbox, Dialog, DialogContent, DialogTitle, List, ListItem, ListItemButton, Typography } from "@mui/material";
 import Slide from '@mui/material/Slide';
 import ChevronRight from "@mui/icons-material/ChevronRight";
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import { iconMap } from "@/routes";
 
 const transition = React.forwardRef(function transition(
     props: TransitionProps & {
@@ -39,7 +40,17 @@ export default function SelectTags(
         handleCloseTagsSelection
     }: SelectTagsProps
 ) {
-    const { data: tagData } = useQuery(GET_TAGS, { fetchPolicy: "cache-first" });
+    const { data: tagData } = useQuery(
+        GET_TAGS,
+        {
+            variables: {
+                sort: {
+                    field: "questionCount",
+                    order: "DESC"
+                }
+            },
+            fetchPolicy: "no-cache"
+        });
     const tagList = tagData?.getTags || [];
 
     const isTagSelected = (tag: string) => {
@@ -84,25 +95,44 @@ export default function SelectTags(
                 <List sx={{ width: "100%" }}>
                     {tagList.map((item: TagValue) => (
                         <ListItem key={item.id}>
-                            <Avatar sx={{ mr: 1, width: "45px", height: "45px" }} />
-                            <Box flexGrow={1}>
-                                <Box display="flex" flexDirection="column">
-                                    <Typography variant="body1" sx={{ fontWeight: "bold", color: "#000" }}>
-                                        # {item.name}
-                                    </Typography>
-                                    <Typography variant="body2" sx={{ color: "#ccc" }}>
-                                        {item.questionCount} discussions
-                                    </Typography>
+                            <ListItemButton
+                                onClick={() => handleSelectTag(item.id)}
+                                sx={{
+                                    borderRadius: "16px"
+                                }}
+                            >
+                                <Avatar
+                                    sx={{
+                                        mr: 1,
+                                        width: "45px",
+                                        height: "45px",
+                                        objectFit: "contain",
+                                        backgroundColor: "#fff"
+                                    }}
+                                >
+                                    <img
+                                        src={iconMap[item.name]}
+                                        style={{ width: "30px", height: "30px", backgroundColor: "#fff" }}
+                                    />
+                                </Avatar>
+                                <Box flexGrow={1}>
+                                    <Box display="flex" flexDirection="column">
+                                        <Typography variant="body1" sx={{ fontWeight: "bold", color: "#000" }}>
+                                            # {item.name}
+                                        </Typography>
+                                        <Typography variant="body2" sx={{ color: "#ccc" }}>
+                                            {item.questionCount} discussions
+                                        </Typography>
+                                    </Box>
                                 </Box>
-                            </Box>
-                            <Checkbox
-                                edge="end"
-                                color="primary"
-                                icon={<RadioButtonUncheckedIcon/>}
-                                checkedIcon={<RadioButtonCheckedIcon/>}
-                                checked={isTagSelected(item.id)}
-                                onChange={() => handleSelectTag(item.id)}
-                            />
+                                <Checkbox
+                                    edge="end"
+                                    color="primary"
+                                    icon={<RadioButtonUncheckedIcon />}
+                                    checkedIcon={<RadioButtonCheckedIcon />}
+                                    checked={isTagSelected(item.id)}
+                                />
+                            </ListItemButton>
                         </ListItem>
                     ))}
                 </List>
---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+export const iconMap: {[key: string]: string} = {
+    "4WD": "/images/road-spikes-solid.svg",
+    "Art": "/images/palette-solid.svg",
+    "Family": "/images/people-robbery-solid.svg",
+    "Fishing": "/images/fish-solid.svg",
+    "Food": "/images/utensils-solid.svg",
+    "House Renting": "/images/people-roof-solid.svg",
+    "Parks": "/images/tree-solid.svg",
+    "Sports": "/images/football-solid.svg",
+    "Vehicles": "/images/car-side-solid.svg",
+};
